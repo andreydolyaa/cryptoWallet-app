0f74f1d333689f11a75ffbb044f3c1dfdc942f2f
Andrey Dolya <dolya7kk@gmail.com>
2020-12-22 21:31:50 +0200

    SDf

--- src/services/bitcoinService.js
+++ src/services/bitcoinService.js
@@ -15,34 +15,32 @@ export const bitcoinService = {
 
 
 async function getRate(coins){
-    // const res = await axios.get(`https://blockchain.info/tobtc?currency=USD&value=${coins}`);
-    // return res.data;
     var crypto = storageService.load(STORAGE_KEY_RATE);
-    // if(!crypto){
-    //     var res = await axios.get(`https://blockchain.info/tobtc?currency=USD&value=${coins}`);
-    //     crypto = res.data
-    //     storageService.store(STORAGE_KEY_RATE,crypto);
-    // }
+    if(!crypto){
+        var res = await axios.get(`https://blockchain.info/tobtc?currency=USD&value=${coins}`);
+        crypto = res.data
+        storageService.store(STORAGE_KEY_RATE,crypto);
+    }
     return crypto;
 }
 
 async function getMarketPrice(){
     var crypto = storageService.load(STORAGE_KEY);
-    // if(!crypto || !crypto.length){
-    //     var res = await axios.get(`https://api.blockchain.info/charts/market-price?timespan=5months&format=json&cors=true`);
-    //     crypto = res.data
-    //     storageService.store(STORAGE_KEY,crypto);
-    // }
+    if(!crypto || !crypto.length){
+        var res = await axios.get(`https://api.blockchain.info/charts/market-price?timespan=5months&format=json&cors=true`);
+        crypto = res.data
+        storageService.store(STORAGE_KEY,crypto);
+    }
     return crypto;
 }
 
 
 async function getConfirmedTransactions(){
     var crypto = storageService.load(STORAGE_KEY_VOLUME);
-    // if(!crypto || !crypto.length){
-    //     var res = await axios.get(`https://api.blockchain.info/charts/trade-volume?timespan=5months&format=json&cors=true`);
-    //     crypto = res.data
-    //     storageService.store(STORAGE_KEY_VOLUME,crypto);
-    // }
+    if(!crypto || !crypto.length){
+        var res = await axios.get(`https://api.blockchain.info/charts/trade-volume?timespan=5months&format=json&cors=true`);
+        crypto = res.data
+        storageService.store(STORAGE_KEY_VOLUME,crypto);
+    }
     return crypto;
 }
